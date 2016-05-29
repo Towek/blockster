d9315c66463ccf91bddf9925444bce745c08d572
Krzysztof Podobinski <towek21@hotmail.com>
2016-05-29 23:25:44 +0200

    working map size dicrease when level change

--- Assets/move.js
+++ Assets/move.js
@@ -10,47 +10,47 @@ function Update () {
     map = GameObject.Find("Map");
     if(Input.GetKeyDown("space")){
         movePower = 2;
-        Debug.Log("Speed BOOST!");
+        //Debug.Log("Speed BOOST!");
     }else if(Input.GetKeyUp("space")){
         movePower = 1;
-        Debug.Log("Speed DOWN!");
+        //Debug.Log("Speed DOWN!");
     }
     if(transform.position.x >= map.transform.lossyScale.x-1 && transform.position.y >= map.transform.lossyScale.y-1){
         if (Input.GetKeyUp ("left")) transform.position.x -= movePower;
         if (Input.GetKeyUp ("down")) transform.position.y -= movePower;
-        Debug.Log("Block up and right");
+        //Debug.Log("Block up and right");
     }else if(transform.position.x <= 0 && transform.position.y >= map.transform.lossyScale.y-1){
         if (Input.GetKeyUp ("right")) transform.position.x += movePower;
         if (Input.GetKeyUp ("down")) transform.position.y -= movePower;
-        Debug.Log("Block up and left");
+        //Debug.Log("Block up and left");
     }else if(transform.position.x <= 0 && transform.position.y <= 0){
         if (Input.GetKeyUp ("up")) transform.position.y += movePower;
         if (Input.GetKeyUp ("right")) transform.position.x += movePower;
-        Debug.Log("Block down and left");
+        //Debug.Log("Block down and left");
     }else if(transform.position.x >= map.transform.lossyScale.x-1 && transform.position.y <= 0){
         if (Input.GetKeyUp ("up")) transform.position.y += movePower;
         if (Input.GetKeyUp ("left")) transform.position.x -= movePower;
-        Debug.Log("Block down and right");
+        //Debug.Log("Block down and right");
     }else if( transform.position.x >= map.transform.lossyScale.x-1){
         if (Input.GetKeyUp ("up")) transform.position.y += movePower;
         if (Input.GetKeyUp ("down")) transform.position.y -= movePower;
         if (Input.GetKeyUp ("left")) transform.position.x -= movePower;
-        Debug.Log("Block right");
+        //Debug.Log("Block right");
     }else if(transform.position.x <= 0){
         if (Input.GetKeyUp ("up")) transform.position.y += movePower;
         if (Input.GetKeyUp ("down")) transform.position.y -= movePower;
         if (Input.GetKeyUp ("right")) transform.position.x += movePower;
-        Debug.Log("Block left");
+        //Debug.Log("Block left");
     }else if(transform.position.y >= map.transform.lossyScale.y-1){
         if (Input.GetKeyUp ("down")) transform.position.y -= movePower;
         if (Input.GetKeyUp ("left")) transform.position.x -= movePower;
         if (Input.GetKeyUp ("right")) transform.position.x += movePower;
-        Debug.Log("Block up");
+        //Debug.Log("Block up");
     }else if(transform.position.y <= 0){
         if (Input.GetKeyUp ("up")) transform.position.y += movePower;
         if (Input.GetKeyUp ("left")) transform.position.x -= movePower;
         if (Input.GetKeyUp ("right")) transform.position.x += movePower;
-        Debug.Log("Block down");
+        //Debug.Log("Block down");
     }else{
         if (Input.GetKeyUp ("up")) transform.position.y += movePower;
         if (Input.GetKeyUp ("down")) transform.position.y -= movePower;
--- Assets/playerPointTrigger.js
+++ Assets/playerPointTrigger.js
@@ -1,7 +1,10 @@
 ﻿#pragma strict
-
+public static var score : int = 0;
+var map : GameObject;
+var lvl_difficultyX = 0;
 function Start () {
-
+    map = GameObject.Find("Map");
+    Debug.Log("Current lvl: "+lvl_difficultyX);
 }
 
 function Update () {
@@ -10,22 +13,29 @@ function Update () {
     var playerPos_x = playerBlock.transform.position.x;
     var playerPos_y = playerBlock.transform.position.y;
     if(playerPos_x == transform.position.x && playerPos_y == transform.position.y){
-        Debug.Log("Working");
+        score++;
+        Debug.Log("Score: "+score);
         generateRandomPosition(transform.position.x, transform.position.y);
+        if(score/2 == lvl_difficultyX){
+            map.transform.position.x -= 0.5;
+            map.transform.position.y -= 0.5;
+            map.transform.localScale.x -= 1;
+            map.transform.localScale.y -= 1;
+            lvl_difficultyX++;
+            Debug.Log("Current lvl: "+lvl_difficultyX);
+        };
     };
 }
 
 function generateRandomPosition(current_x, current_y){
-    var map : GameObject;
-    map = GameObject.Find("Map");
-    var map_x = map.transform.lossyScale.x;
-    var map_y = map.transform.lossyScale.y;
-    var rand_x = Mathf.Round(Random.Range(0, map_x-1));
-    var rand_y = Mathf.Round(Random.Range(0, map_y-1));
+    var map_x = map.transform.localScale.x;
+    var map_y = map.transform.localScale.y;
+    var rand_x = Mathf.Round(Random.Range(0, map_x-2));
+    var rand_y = Mathf.Round(Random.Range(0, map_y-2));
     if (rand_x != current_x || rand_y != current_y){
         transform.position.y = rand_y;
         transform.position.x = rand_x;
-        Debug.Log("New point pos x:"+ rand_x + "y: " + rand_y);
+        //Debug.Log("New point pos x:"+ rand_x + "y: " + rand_y);
     }else{
         transform.position.x = Random.Range(0, map_x);
         transform.position.y = Random.Range(0, map_y);
