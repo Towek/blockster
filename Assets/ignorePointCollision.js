be824e56b132112145980fa4b91c79f91e4588aa
Krzysztof Podobinski <towek21@hotmail.com>
2016-05-29 20:29:12 +0200

    fixed point player collision

--- Assets/ignorePointCollision.js
+++ Assets/ignorePointCollision.js
@@ -2,7 +2,7 @@
 var bulletPrefab : Transform;
 function Start () {
     var bullet = Instantiate(bulletPrefab) as Transform;
-    Physics.IgnoreCollision(bullet.GetComponent.<Collider>(), GetComponent.<Collider>());
+    Physics2D.IgnoreCollision(bullet.GetComponent.<Collider2D>(), GetComponent.<Collider2D>());
 }
 
 function Update () {
