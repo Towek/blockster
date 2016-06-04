5dcde04d8fca470504d5c19275714591d2d1ea90
Krzysztof Podobinski <towek21@hotmail.com>
2016-06-05 01:12:06 +0200

    move animation

--- Assets/destroyOnExitCamera.js
+++ Assets/destroyOnExitCamera.js
@@ -6,11 +6,12 @@ function Start () {
 
 function Update () {
     var removeLaser = Camera.main.orthographicSize*2+Camera.main.transform.position.x/2;
-    clone = GameObject.Find("enemyClone");
-    if(clone.transform.position.x > removeLaser){
-        //var enemysAlive = enemySpawner.enemyMaxSlots-enemySpawner.enemyFreeSlots;
-        //Debug.Log("Permanently alive enemys on map from "+(enemysAlive)+" to "+(enemysAlive-1));
-        Destroy(clone);
-        //enemySpawner.enemyFreeSlots++;
+    
+    if(GameObject.Find("enemyClone") != -1){
+        clone = GameObject.Find("enemyClone");
+        if(clone.transform.position.x > removeLaser){
+            Destroy(clone);
+        }
     }
+
 }
--- Assets/move.js
+++ Assets/move.js
@@ -1,11 +1,12 @@
 ﻿#pragma strict
 var movePower = 1;
+var anim : Animator;
 function Start () {
 
 }
 
 function Update () {
-   
+    anim = GetComponent(Animator);
     var map : GameObject;
     map = GameObject.Find("Map");
     if(Input.GetKeyDown("space")){
@@ -52,7 +53,7 @@ function Update () {
         if (Input.GetKeyUp ("right")) transform.position.x += movePower;
         //Debug.Log("Block down");
     }else{
-        if (Input.GetKeyUp ("up")){ transform.position.y += movePower;  animation.Play("moveY");;}
+        if (Input.GetKeyUp ("up")){  anim.Play("moveY", PlayMode.StopSameLayer);}
         if (Input.GetKeyUp ("down")) transform.position.y -= movePower;
         if (Input.GetKeyUp ("left")) transform.position.x -= movePower;
         if (Input.GetKeyUp ("right")) transform.position.x += movePower;
