0da2536d098158309a696a7d44299f5b9d38582e
Krzysztof Podobinski <towek21@hotmail.com>
2016-06-08 23:43:46 +0200

    commit metafiles

---
+++ Assets/playerPointTrigger.js
@@ -0,0 +1,86 @@
+﻿#pragma strict
+public static var score : int = 0;
+var map : GameObject;
+var lvl_difficultyX = 1;
+var spawner: GameObject;
+function Start () {
+    map = GameObject.Find("Map");
+    Debug.Log("Current lvl: "+lvl_difficultyX);
+    generateRandomPosition(transform.position.x, transform.position.y);
+}
+
+function Update () {
+    //Debug.Log();
+    var playerBlock : GameObject;
+    playerBlock = GameObject.Find("playerBlock");
+    var playerPos_x = playerBlock.transform.position.x;
+    var playerPos_y = playerBlock.transform.position.y;
+    if(playerPos_x == transform.position.x && playerPos_y == transform.position.y){
+        score++;
+        Debug.Log("Score: "+score);
+        generateRandomPosition(transform.position.y, transform.position.y);
+        if(score/2 == lvl_difficultyX){
+            if(lvl_difficultyX <= 16){
+                Camera.main.transform.position.x -= 0.5;
+                Camera.main.transform.position.y -= 0.5;
+                Camera.main.orthographicSize -= 0.5;
+                map.transform.position.x -= 0.5;
+                map.transform.position.y -= 0.5;
+                map.transform.localScale.x--;
+                map.transform.localScale.y--;
+                lvl_difficultyX++;
+                Debug.Log("Current lvl: "+lvl_difficultyX);
+                
+
+                var enemySpawner : enemySpawner = map.GetComponent(typeof(enemySpawner));
+                enemySpawner.spawnSpeed -= 0.005*(score/10);
+                enemySpawner.cloneSpeed += 0.1;
+                enemySpawner.spawnPlace = [];
+                //Debug.Log (MaxValue(enemySpawner.spawnPlace));
+                enemySpawner.randomPosition();
+                enemySpawner.RandomizeArray(enemySpawner.spawnPlace);
+            }
+        }
+    }
+}
+
+
+function generateRandomPosition(current_x, current_y){
+    var rand_x;
+    var rand_y;
+    if(lvl_difficultyX > 16){
+        rand_x = Mathf.Round(Random.Range(1, map.transform.localScale.x-1));
+        rand_y = Mathf.Round(Random.Range(1, map.transform.localScale.y-1));
+        if (rand_x != current_x || rand_y != current_y){
+            transform.position.y = rand_y;
+            transform.position.x = rand_x;
+            Debug.Log("New point pos x:"+ rand_x + "y: " + rand_y);
+        }else{
+            transform.position.x = Mathf.Round(Random.Range(1, map.transform.localScale.x-1));
+            transform.position.y = Mathf.Round(Random.Range(1, map.transform.localScale.y-1));
+        }
+    }else{
+        rand_x = Mathf.Round(Random.Range(1, map.transform.localScale.x-2));
+        rand_y = Mathf.Round(Random.Range(1, map.transform.localScale.y-2)) ;
+        if (rand_x != current_x || rand_y != current_y){
+            transform.position.y = rand_y;
+            transform.position.x = rand_x;
+            Debug.Log("New point pos x:"+ rand_x + "y: " + rand_y);
+        }else{
+            transform.position.x = Mathf.Round(Random.Range(1, map.transform.localScale.x-2));
+            transform.position.y = Mathf.Round(Random.Range(1, map.transform.localScale.y-2));
+        }
+    }
+}
+
+function MaxValue (intArray : int[]) : int {
+    var max = intArray[0];
+    var location = 0;
+    for (var i = 1; i < intArray.Length; i++) {
+        if (intArray[i] > max) {
+            max = intArray[i];
+            location = i;
+        }
+    }
+    return location;
+}
